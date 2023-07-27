5c2d72e6d5c7d36dbd476d26d4d8540c68cacb71
Chon-Malai <chon.malai@gmail.com>
2023-07-27 16:09:58 +0700

    fix:debug_responsive

--- src/components/homePage/Herosection.tsx
+++ src/components/homePage/Herosection.tsx
@@ -9,20 +9,17 @@ import { buttonVariants } from "../ui/Button";
 import { useAuthContext } from "@/contexts/authContext";
 import { FC } from "react";
 
-interface ILogin {
-  isLoggedIn: boolean;
-}
 
-const Herosection: FC<ILogin> = () => {
+const Herosection: FC= () => {
   const { isLoggedIn } = useAuthContext();
 
   return (
     // <div className="min-h-screen">
     <div
       id="hero-section"
-      className="flex xl:h-[calc(100vh-70px)] justify-center xl:max-w-6xl mx-auto xl:items-center xl:my-0 text-gray-900 my-14 "
+      className="flex lg:h-[calc(100vh-70px)] justify-center lg:max-w-6xl mx-auto lg:items-center lg:my-0 text-gray-900 my-14 "
     >
-      <div className="flex xl:flex-row xl:justify-between items-center gap-12 mx-auto flex-col ">
+      <div className="flex lg:flex-row lg:justify-between items-center gap-12 mx-auto flex-col ">
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -37,16 +34,16 @@ const Herosection: FC<ILogin> = () => {
             hidden: { opacity: 0, x: -150 },
             visible: { opacity: 1, x: 0 },
           }}
-          className="flex flex-col xl:w-6/12 xl:items-start gap-7 items-center "
+          className="flex flex-col lg:w-6/12 lg:items-start gap-7 items-center "
         >
-          <div className="flex xl:flex-col sm:flex-row flex-col xl:gap-4 xl:text-5xl xl:font-extrabold text-3xl gap-2 font-semibold ">
+          <div className="flex lg:flex-col sm:flex-row lg:gap-4 lg:text-5xl md:text-3xl lg:font-extrabold text-3xl gap-2 font-semibold ">
             <div className="flex gap-2 flex-row ">
               <h1 className="flex ">เริ่มทำระบบ</h1>
               <h1 className="flex text-primary-button ">Factory 4.0</h1>
             </div>
-            <h1 className="xl:text-left text-center">ด้วยตัวเอง</h1>
+            <h1 className="lg:text-left text-center">ด้วยตัวเอง</h1>
           </div>
-          <div className="flex xl:w-full xl:flex-col xl:justify-start flex-wrap xl:text-2xl leading-7 font-normal xl:gap-3 flex-row gap-0 justify-center w-3/4 ">
+          <div className="flex lg:w-full lg:flex-col lg:justify-start flex-wrap lg:text-2xl leading-7 font-normal lg:gap-3 flex-row gap-0 justify-center w-3/4 ">
             <p>เริ่มตั้งแต่สอนเขียน C# ไม่จำเป็นต้อง</p>
             <p>มีพื้นฐานการเขียนโปรแกรม เรียนแบบ</p>
             <p>On-demand สัปดาห์ละ 2 วิดีโอ ในกลุ่ม</p>
@@ -96,7 +93,7 @@ const Herosection: FC<ILogin> = () => {
             hidden: { opacity: 0, x: 150 },
             visible: { opacity: 1, x: 0 },
           }}
-          className="xl:w-1/2 w-3/4"
+          className="lg:w-1/2 w-3/4"
         >
           <Image
             src={heroImage}
